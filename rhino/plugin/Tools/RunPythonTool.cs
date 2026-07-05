901d5f221c0b4aefb549be95beac2dcf4e6f089d
Callum Sykes <callumsykes@protonmail.com>
2026-07-05 11:39:45 -0700

    Add _ to Run Script

--- rhino/plugin/Tools/RunPythonTool.cs
+++ rhino/plugin/Tools/RunPythonTool.cs
@@ -15,7 +15,7 @@ public static class RunPythonTool
         var tmp = Path.Combine(Path.GetTempPath(), $"rhino_mcp_{Guid.NewGuid():N}.py");
         File.WriteAllText(tmp, script);
         RhinoApp.CommandWindowCaptureEnabled = true;
-        RhinoApp.RunScript(doc.RuntimeSerialNumber, $"-ScriptEditor _Run \"{tmp}\"", false);
+        RhinoApp.RunScript(doc.RuntimeSerialNumber, $"_-ScriptEditor _Run \"{tmp}\"", false);
         string[] lines = RhinoApp.CapturedCommandWindowStrings(true);
         RhinoApp.CommandWindowCaptureEnabled = false;
         _ = Task.Delay(15_000).ContinueWith(_ => { try { File.Delete(tmp); } catch { } });
